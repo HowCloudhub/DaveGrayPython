cf3089d1c7d81445a1aab2fc3355dd75cca4bf60
HowCloudhub <howlader101@gmail.com>
2026-02-21 20:19:58 -0500

    First commit to Dave Gray repo

---
+++ Various/various.py
@@ -0,0 +1,38 @@
+
+
+# dictionary = {"Munir": 5,
+#               "Davind": 9,
+#               "Tom": 10,
+#               "Saleh": 20
+#               }
+
+# print(f"Number of items in Dictionary is:  {len(dictionary)}")
+
+# for x in dictionary.values():
+#     print((x))
+
+
+# food = input("Enter your favourite food (press 1 to quit): ")
+
+# while food != "1":
+#     print(f"Your favourite food is {food}")
+#     food = input("Enter your favourite food (press 1 to quit): ")
+
+# print("You pressed 1, you are not nice. Bye")
+
+# age = int(input("Enter Your Age: "))
+
+# while age < 18:
+#     print("You must be over 18 to play.")
+#     age = int(input("Enter Your Age: "))
+
+
+# print(f"You are {age} years old")
+
+
+temp = int(input("What's the temperature outside? :"))
+
+if temp >= 10 and temp <= 30:
+    print("The weather is good!!")
+else:
+    print("Weather is bad")
