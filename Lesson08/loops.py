c05513ab7087875fed3575a19e7e4c60a3d30cae
HowCloudhub <howlader101@gmail.com>
2026-02-23 21:43:10 -0500

    Upload lesson 8)

---
+++ Lesson08/loops.py
@@ -0,0 +1,61 @@
+# loops
+# while Loop
+# value = 1
+# while value <= 10:
+#     print(value)
+#     if value == 5:
+#         break
+#     value += 1
+
+
+# value = 0
+# while value <= 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print('Value is now eqaul to ' + str(value))
+
+# print('=========================')
+# for i in range(0, 10):
+#     print(f"Loop no. {i + 1} , i value = {i}")
+
+
+names = ['Dave', 'Sara', 'Jhon']
+# for x in names:
+#     print(x)
+
+# for x in 'Mississippi':
+#     print(x)
+
+# for x in names:
+#     if x == "Sara":
+#         break
+#     print(x)
+
+for x in names:
+    if x == "Sara":
+        continue
+    print(x)
+
+
+# for x in range(4):
+#     print(x)
+# for x in range(2, 4):
+#     print(x)
+for x in range(0, 101, 5):  # increment by 5
+    print(x)
+else:
+    print("Glad that's over")
+
+
+names = ['Dave', 'Sara', 'Jhon']
+actions = ['codes', 'eats', 'sleeps']
+
+# for name in names:
+#     for action in actions:
+#         print(name + " " + action + ".")
+for action in actions:
+    for name in names:
+        print(name + " " + action + ".")
