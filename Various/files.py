95c8a9b97c2fbc4378c5b8bcb43a944c4a1f5113
HowCloudhub <howlader101@gmail.com>
2026-02-25 21:59:27 -0500

    updating some files

--- Various/files.py
+++ Various/files.py
@@ -21,3 +21,17 @@ writeToLog('log.txt', f"{timestamp} - {name}")
 
 print("Log Contents")
 print(readLog("log.txt"))
+
+x = input("What is the value of x? ")
+y = input('What is the value of y? ')
+z = input('What is the value of z? ')
+
+if x < y:
+    print(f'{x} is more than {y}')
+
+if x > y:
+    print(f'{x} is less than {y}')
+
+if x > y:
+    if y < z:
+        print(f"While {x} is more than {y}, {y} is less than {z}")
---
+++ Various/madlibs.py
@@ -0,0 +1,24 @@
+
+adj1 = input(f"Enter an adjective(like good/bad/awesome/stupid): ")
+adj2 = input(
+    f"Enter another adjective(like beautiful/ugly/sweet/hungry/stupid): ")
+adj3 = input(
+    f"Enter another adjective(like smart/dumb/awesome/stupid/beautiful): ")
+event1 = input(
+    f"Enter an daytime or event (Like a day, night, storm, nightmare): ")
+verb1 = input("Enter a verb (like run, walk, jump, fly etc): ")
+animal = input("Enter your favourite animal: ")
+
+
+print("===================================")
+print("===================================")
+print("###### Here is your true story ######")
+print("===================================")
+print("===================================")
+print(f"Yesterday was a {adj1} day. I woke up in the \
+    middle of the {event1} and started to {verb1}. \
+    Later in the day I saw a {animal}, it was just\
+    {adj2}. No one beleive my story and they think\
+    I am {adj3}. ")
+print("===================================")
+print("===================================")
